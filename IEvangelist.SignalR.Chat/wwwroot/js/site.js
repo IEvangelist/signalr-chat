1dea523ffd3afe1893f6257cc5faa2a1403ba901
David Pine <david.pine.7@gmail.com>
2019-02-19 22:12:21 -0600

    Heyo!

--- IEvangelist.SignalR.Chat/wwwroot/js/site.js
+++ IEvangelist.SignalR.Chat/wwwroot/js/site.js
@@ -62,6 +62,10 @@
                 this.message = json.text;
                 this.messageId = json.id;
                 $(':text').focus();
+            },
+            appendToMessage(text) {
+                this.message += text;
+                $(':text').focus();
             }
         }
     });
