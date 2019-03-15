cf1e639875ff211077e212f46d5f40d628948096
David Pine <david.pine.7@gmail.com>
2019-03-15 09:21:14 -0500

    Added strongly type chat hub. It's within the existing hub for demo purposes.

--- IEvangelist.SignalR.Chat/Hubs/ChatHub.cs
+++ IEvangelist.SignalR.Chat/Hubs/ChatHub.cs
@@ -74,4 +74,57 @@ namespace IEvangelist.SignalR.Chat.Hubs
 
         Task MessageReceived(object args);
     }
+
+    public class StronglyTypedChatHub : Hub<IChatHub>
+    {
+        string Username => Context.User.Identity.Name;
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.MessageReceived(
+                new
+                {
+                    text = $"💯 Hi, {Username}! This chat application is powered by SignalR 👍🏽",
+                    id = "greeting",
+                    isGreeting = true,
+                    user = "👋"
+                });
+
+            await Clients.Others.UserLoggedOn(
+                new
+                {
+                    user = Username
+                });
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+            => await Clients.Others.UserLoggedOff(
+                new
+                {
+                    user = Username
+                });
+
+        public async Task PostMessage(string message, string id = null)
+            => await Clients.All.MessageReceived(
+                new
+                {
+                    text = message,
+                    id = UseOrCreateId(id),
+                    isEdit = id != null,
+                    user = Username
+                });
+
+        public async Task UserTyping(bool isTyping)
+            => await Clients.Others.UserTyping(
+                new
+                {
+                    isTyping,
+                    user = Username
+                });
+
+        static string UseOrCreateId(string id)
+            => string.IsNullOrWhiteSpace(id)
+                ? Guid.NewGuid().ToString()
+                : id;
+    }
 }
