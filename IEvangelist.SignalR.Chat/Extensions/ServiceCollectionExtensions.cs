00e0142862455960676403ab13609728245bdd25
David Pine <david.pine.7@gmail.com>
2019-05-06 06:31:38 -0500

    Clean up

---
+++ IEvangelist.SignalR.Chat/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using IEvangelist.SignalR.Chat.Bots;
+using IEvangelist.SignalR.Chat.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureCookiePolicy(
+            this IServiceCollection services)
+            => services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+        public static IServiceCollection AddChatAuthentication(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            _ = services.AddAuthentication("Cookies")
+                        .AddCookie()
+                        .AddTwitter(o => configuration.GetSection("Authentication:Twitter").Bind(o))
+                        .AddGoogle(o =>
+                        {
+                            o.ClientId = configuration["Authentication:Google:ClientId"];
+                            o.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+                            o.UserInformationEndpoint = "https://www.googleapis.com/oauth2/v2/userinfo";
+                            o.ClaimActions.Clear();
+                            o.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                            o.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                            o.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "given_name");
+                            o.ClaimActions.MapJsonKey(ClaimTypes.Surname, "family_name");
+                            o.ClaimActions.MapJsonKey("urn:google:profile", "link");
+                            o.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+                        });
+
+            return services;
+        }
+
+        public static IServiceCollection AddChatServices(
+            this IServiceCollection services)
+        {
+            services.AddSingleton<ICommandSignal, CommandSignal>();
+            services.AddHttpClient<IDadJokeService, DadJokeService>(
+                client => client.DefaultRequestHeaders.Add("Accept", "text/plain"));
+            return services.AddHostedService<ChatBotService>();
+        }
+    }
+}
--- IEvangelist.SignalR.Chat/Startup.cs
+++ IEvangelist.SignalR.Chat/Startup.cs
@@ -1,12 +1,6 @@
-using System.Net;
-using System.Security.Claims;
-using IEvangelist.SignalR.Chat.Bots;
 using IEvangelist.SignalR.Chat.Hubs;
-using IEvangelist.SignalR.Chat.Services;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,35 +15,13 @@ namespace IEvangelist.SignalR.Chat
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            services.AddMvc()
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddAuthentication("Cookies")
-                    .AddCookie()
-                    .AddTwitter(options => _configuration.GetSection("Authentication:Twitter").Bind(options))
-                    .AddGoogle(o =>
-                     {
-                         o.ClientId = _configuration["Authentication:Google:ClientId"];
-                         o.ClientSecret = _configuration["Authentication:Google:ClientSecret"];
-                         o.UserInformationEndpoint = "https://www.googleapis.com/oauth2/v2/userinfo";
-                         o.ClaimActions.Clear();
-                         o.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-                         o.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-                         o.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "given_name");
-                         o.ClaimActions.MapJsonKey(ClaimTypes.Surname, "family_name");
-                         o.ClaimActions.MapJsonKey("urn:google:profile", "link");
-                         o.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
-                     });
+            services.ConfigureCookiePolicy();
+            services.AddChatAuthentication(_configuration);
+            services.AddChatServices();
 
-            services.AddHostedService<ChatBotService>();
-            services.AddSingleton<ICommandSignal, CommandSignal>();
-            services.AddHttpClient<IDadJokeService, DadJokeService>(
-                client => client.DefaultRequestHeaders.Add("Accept", "text/plain"));
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSignalR(options => options.EnableDetailedErrors = true)
                     .AddAzureSignalR();
         }
@@ -71,11 +43,9 @@ namespace IEvangelist.SignalR.Chat
                .UseStaticFiles()
                .UseCookiePolicy()
                .UseAuthentication()
-               //.UseSignalR(routes => routes.MapHub<ChatHub>("/chat"))
+               // Gone with .NET Core 3.0
                .UseAzureSignalR(routes => routes.MapHub<ChatHub>("/chat"))
                .UseMvc();
-
-            // Gone with .NET Core 3.0
         }
     }
 }
