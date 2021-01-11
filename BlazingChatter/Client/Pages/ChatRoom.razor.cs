1565f45760eeb3b6c00678ae59f2265500b3d3fe
David Pine <david.pine@microsoft.com>
2021-01-11 08:21:15 -0600

    A bit closer now... need to fix focus, add speech and JS interop

--- BlazingChatter/Client/Pages/ChatRoom.razor.cs
+++ BlazingChatter/Client/Pages/ChatRoom.razor.cs
@@ -15,7 +15,7 @@ namespace BlazingChatter.Client.Pages
         HubConnection _hubConnection;
         string _messageId;
         string _message;
-        string[] _typingUsers;
+        string[] _usersTyping;
         bool _isTyping;
 
         [Parameter]
@@ -34,6 +34,36 @@ namespace BlazingChatter.Client.Pages
             _hubConnection.On<ActorMessage>("MessageReceived", OnMessageReceivedAsync);
 
             await _hubConnection.StartAsync();
+        }
+
+        async Task SendMessage()
+        {
+            if (_message is { Length: > 0 })
+            {
+                await _hubConnection.InvokeAsync("PostMessage", _message, _messageId);
+
+                _message = null;
+                _messageId = null;
+
+                StateHasChanged();
+            }
+        }
+
+        async Task SetIsTyping(bool isTyping)
+        {
+            if (_isTyping && isTyping)
+            {
+                return;
+            }
+
+            await _hubConnection.InvokeAsync("UserTyping", _isTyping = isTyping);
+        }
+
+        async Task AppendToMessage(string text)
+        {
+            _message += text;
+
+            await SetIsTyping(false);
         }
 
         async Task OnMessageReceivedAsync(ActorMessage message)
