7393f3c936840cd3c16451c1bdbd6663cfe49200
IEvangelist <david.pine.7@gmail.com>
2021-01-10 19:09:19 -0600

    static locals

--- BlazingChatter/Server/Startup.cs
+++ BlazingChatter/Server/Startup.cs
@@ -84,12 +84,12 @@ namespace BlazingChatter.Server
 
             app.UseEndpoints(endpoints =>
             {
-                static Task register(HttpContext context) =>
+                static Task RegisterTask(HttpContext context) =>
                     Task.Factory.StartNew(
                         () => context.Response.Redirect("/Identity/Account/Login", true, true));
 
-                endpoints.MapGet("/Identity/Account/Register", register);
-                endpoints.MapPost("/Identity/Account/Register", register);
+                endpoints.MapGet("/Identity/Account/Register", RegisterTask);
+                endpoints.MapPost("/Identity/Account/Register", RegisterTask);
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
                 endpoints.MapFallbackToFile("index.html");
